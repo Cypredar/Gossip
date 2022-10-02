28b5ea8b5861cdcd25af64163565d305e0a9efb2
ztj <58116272+Cypredar@users.noreply.github.com>
2022-10-03 00:54:49 +0800

    Update a.py

--- a.py
+++ a.py
@@ -2,16 +2,16 @@ system = System()
 
 system.clk_domain = SrcClockDomain()
 system.clk_domain.clock = '1GHz'
-system.clk_domain.voltage_domain = VoltageDomain()
+system.clk_domain.voltage_domain = VoltageDomain() # clock
 
 system.mem_mode = 'timing'
-system.mem_ranges = [AddrRange('512MB')]
+system.mem_ranges = [AddrRange('512MB')] # memory
 
-system.cpu = TimingSimpleCPU()
+system.cpu = TimingSimpleCPU() # cpu
 
 system.membus = SystemXBar()
 
-system.cpu.icache_port = system.membus.cpu_side_ports
+system.cpu.icache_port = system.membus.cpu_side_ports # port
 system.cpu.dcache_port = system.membus.cpu_side_ports
 system.cpu.icache_port = system.l1_cache.cpu_side
 system.cpu.icache_port = system.membus.cpu_side_ports
@@ -19,9 +19,9 @@ system.cpu.createInterruptController()
 system.system_port = system.membus.cpu_side_ports
 system.mem_ctrl = MemCtrl()
 
-system.workload = SEWorkload.init_compatible(binary)
+system.workload = SEWorkload.init_compatible(binary) # os
 
-binary = 'tests/test-progs/hello/bin/x86/linux/hello'
+binary = 'tests/test-progs/hello/bin/x86/linux/hello' # executable
 process = Process()
 process.cmd = [binary]
 system.cpu.workload = process
@@ -30,4 +30,4 @@ system.cpu.createThreads()
 root = Root(full_system = False, system = system)
 m5.instantiate()
 
-exit_event = m5.simulate()
+exit_event = m5.simulate() # start simulation
